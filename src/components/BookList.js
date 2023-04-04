e53cacf1428862b39f89730c96b34e9032ebd98f
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-04-04 06:24:51 +0200

    passing the book list

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,7 @@
 import BookShow from './BookShow';
 function BookList({ books }) {
   return (
-    <div>
+    <div className="book-list">
       {books.map((book) => (
         <BookShow book={book} key={book.id} />
       ))}
