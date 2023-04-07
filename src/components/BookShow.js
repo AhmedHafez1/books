f158e7d46d2a17c5d3ec6cdd0e93ad0cfa3194a7
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-04-07 06:08:49 +0200

    init handleSubmit

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -4,13 +4,13 @@ import BookEdit from './BookEdit';
 function BookShow({ book, onDelete, onEdit }) {
   const [showEdit, setShowEdit] = useState(false);
 
-  let content = <h3>{book.title}</h3>;
-  if (showEdit) content = <BookEdit book={book} onSubmit={handleSubmit} />;
-
   const handleSubmit = (id, title) => {
     onEdit(id, title);
     setShowEdit(false);
   };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) content = <BookEdit book={book} onSubmit={handleSubmit} />;
 
   return (
     <div className="book-show">
