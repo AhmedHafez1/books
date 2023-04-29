81440f393ea7ade228a4fe42f4fa4c00f0a4b1c2
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-04-29 17:36:39 +0300

    remove count from context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,8 +3,6 @@ import BookShow from './BookShow';
 import { useContext } from 'react';
 
 function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BookContext);
-
   const renderedBooks = books.map((book) => {
     return (
       <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
@@ -12,8 +10,6 @@ function BookList({ books, onDelete, onEdit }) {
   });
   return (
     <div>
-      <div>{count}</div>
-      <button onClick={incrementCount}>Inc</button>
       <div className="book-list">{renderedBooks}</div>
     </div>
   );
--- src/context/book.js
+++ src/context/book.js
@@ -3,13 +3,7 @@ import { createContext, useState } from 'react';
 const BookContext = createContext();
 
 export const Provider = ({ children }) => {
-  const [count, setCount] = useState(5);
-  const value = {
-    count,
-    incrementCount: () => setCount(count + 1),
-  };
-
-  return <BookContext.Provider value={value}>{children}</BookContext.Provider>;
+  return <BookContext.Provider value={{}}>{children}</BookContext.Provider>;
 };
 
 export default BookContext;
